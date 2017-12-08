fd0818754d56a01624380fe6f605c532986b8d47
HaraHeique <heikacademicos@hotmail.com>
2017-12-08 11:46:33 -0200

    Update checa aí

--- StoresProject_version2/src/app/MainApp.java
+++ StoresProject_version2/src/app/MainApp.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.LinkedList;
+
 import modelStore.*; 
 
 public class MainApp {
@@ -119,15 +121,18 @@ public class MainApp {
     	return itens;
     }
     
+    /* Menu que interage com o usuário final do sistema */
     public static void menu (Map<String, Loja> lojas, List<Item> itens) 
     {
     	//Menu para o usuario que chamar a classe aplicacao
         
     	// Inicialiação das variaveis
+    	List<Carrinho> compras = new LinkedList<>();
         List<ItemCarrinho> cart = new ArrayList<>();
         List<Item> itensFiltrados = null;
         int opcao = 0;
-        boolean chamadaCompra = false; 
+        boolean chamadaCompra = false;
+        String msgErro = "Entrada de dado inválida. Tente novamente:"; 
 		
         //Faz o menu enquanto o opcao continuar ser menor que zero
         do{
@@ -141,22 +146,25 @@ public class MainApp {
         	System.out.println("Escolha uma opcao da busca desejada dos itens:");
 			System.out.println("1 - Por Loja\n2 - Por Nome do produto\n3 - Por Tipo\n4 - Código do produto"
 					+ "\n5 - Todos os itens do sistema\n6 - Alterar carrinho (Em construção)\n0 - Encerrar Sistema");
-
-			opcao = Integer.parseInt(scanUser.nextLine());
+			
+			opcao = controleEntradaDados(scanUser.nextLine(), 0, 100, msgErro, msgErro);
 			
         	Listagem.pularLinha(2);
         	switch (opcao) {
         		case 0:
         			System.out.println("Agradecemos a preferência!!\n VOLTE SEMPRE");
         			chamadaCompra = false;
+        			
         			break;
         			
         		case 1: //busca por loja
         			List<Loja> listLojas = Busca.todasLojas(lojas);
+        			System.out.println("0 - Voltar");
         			Listagem.listarLojas(listLojas);
-        			System.out.println("Selecione a loja desejada: ");
-        			opcao = Integer.parseInt(scanUser.nextLine()); // Captura a opcao desejada da loja
-        			itensFiltrados = Busca.porLoja(itens, listLojas.get(opcao-1));
+        			System.out.println("\nSelecione a loja desejada: ");
+        			int opcao2 = controleEntradaDados(scanUser.nextLine(), 0, listLojas.size(), msgErro, msgErro); // Captura a opcao desejada da loja
+        			if (opcao2 == 0) { break; } 
+        			itensFiltrados = Busca.porLoja(itens, listLojas.get(opcao2 - 1));
         			chamadaCompra = true;
         			break;
         		
@@ -194,8 +202,9 @@ public class MainApp {
         			if (cart.isEmpty()) {
 						System.out.println("Carrinho Vazio");
 					}else {
-	        			menuCart(itens,cart);
+	        			menuCart(itens, cart, compras);
 					}
+        			chamadaCompra = false;
         			break;
         			
         		default:
@@ -214,103 +223,181 @@ public class MainApp {
     }
     
     //Método que serve para o usuário do sistema comprar itens cadastrados no sistema
-    private static boolean tcgBuy(List<Item> itens, List<ItemCarrinho> cart) 
+    private static void tcgBuy(List<Item> itens, List<ItemCarrinho> cart) 
     {
     	Listagem.pularLinha(2);
-    	// Inicialização das variaveis
-    	Item itemSelecionado = null;
-    	int decision = 0;
-    	int opcao = 0;
+    	int decision = 1;
     	
-    	// Pergunta para o usuário se ele realmente deseja comprar um dos pordutos (Adicionar no carrinho)
-    	System.out.println("Você deseja: \n1 - Comprar algum item"
-							+ "\n2 - Voltar para o menu principal");
-    	decision = controleEntradaDados(scanUser.nextLine(), 1, 2);
-    	Listagem.pularLinha(2);
-    	//Deseja comprar
-		if (decision == 1) {			
-			System.out.println("Selecione o item que deseja comprar:");
-			// Lista o resultado da pesquisa outra
-			Listagem.listarItens(itens);
-			Listagem.pularLinha(1);
-			
-			opcao = controleEntradaDados(scanUser.nextLine(), 1, itens.size()) - 1; // captura o nome da loja
-			
-			// atraves do que foi selecionado pelo usuario é colocado o item em seu carrinho
-			itemSelecionado = itens.get(opcao); 
-			
-			// Verifica se foi encontrado algum produto
-			if (itemSelecionado != null) {
-				int qtd = 0;
-				// Garantir que o usuario digite um valor que seja aceito.
-				do {
-					System.out.println("Informe a quantidade do produto: ");
-					qtd = Integer.parseInt(scanUser.nextLine());
-				}while(!(itemSelecionado.debitarEstoque(qtd))); // Remove a quantidade do estoque geral momentaneamente;
+    	while (decision == 1) {
+    		// Pergunta para o usuário se ele realmente deseja comprar um dos pordutos (Adicionar no carrinho)
+        	System.out.println("Você deseja: \n1 - Comprar algum item"
+    							+ "\n2 - Voltar para o menu principal");
+        	
+        	String msgErro = "Entrada de dado inválida. Tente novamente:";
+        	
+        	decision = controleEntradaDados(scanUser.nextLine(), 1, 2, msgErro, msgErro);
+        	Listagem.pularLinha(2);
+        	
+        	if (decision == 2) //O usuário deseja voltar para o menu principal
+        		return;
+        	
+        	int opcao = 1;
+        	//Deseja comprar	
+			while (opcao != 0) {
+				System.out.println("Selecione o item que deseja comprar:");
+				// Lista o resultado da pesquisa outra
+				System.out.println("0 - Voltar");
+				Listagem.listarItens(itens);
+				Listagem.pularLinha(1);
 				
-				// Insere o produto no carrinho
-				cart.add(new ItemCarrinho(itemSelecionado, qtd));
+				//Escolher opção de escolha
+				opcao = controleEntradaDados(scanUser.nextLine(), 0, itens.size(), msgErro, msgErro);
 				
-				System.out.println("Produto adicionado ao Carrinho");
+				// atraves do que foi selecionado pelo usuario é colocado o item em seu carrinho
+				if (opcao != 0) {
+					Item itemSelecionado = itens.get(opcao-1);
+					
+				// Verifica se não tem mais o item no estoque
+					if (itemSelecionado.getQuantidade() > 0) {
+						
+						String msgErro2 = "Quantidade inserida inválida ou indisponível no estoque. Insira novamente:";
+						
+						System.out.println("Informe a quantidade do produto: ");
+						int qtd = controleEntradaDados(scanUser.nextLine(), 1, itemSelecionado.getQuantidade(), msgErro, msgErro2);
+						itemSelecionado.debitarEstoque(qtd);
+						ItemCarrinho itemCart = new ItemCarrinho(itemSelecionado, qtd); 
+						
+						// Verifica primeiro se já existe o item no carrinho e caso tenha só precisa adicionar a quantidade escolhida
+						if (cart.contains(itemCart)) {
+							int pos = cart.indexOf(itemCart);
+							cart.get(pos).adicionaQtdItens(qtd);
+						}
+						else { cart.add(itemCart); } // Adiciona o item que não existe no item carrinho ainda
+						
+						System.out.println("Produto adicionado ao Carrinho");
+						
+					}
+					else //Caso o estoque do item esteja zerado
+						System.out.println("Não há disponibilidade do item selecionado no estoque");
+					
+					Listagem.pularLinha(1);
+				}
 			}
-			else {
-				System.out.println("Ocorreu algum erro tente de novo.");
-			}
-			return true;
 		}
-		
-		//Nao deseja comprar
-		return false;
     }
 
     // Menu para interagir com o carrinho.
-    private static void menuCart(List<Item> itens, List<ItemCarrinho> cart) {
-    	
-    	int nav = 0;
-    	
-    	do{	
+    private static void menuCart(List<Item> itens, List<ItemCarrinho> cart, List<Carrinho> compras) 
+    {
+    	int opcao1, opcao2, quantidade;
+    	do {	
     		System.out.println("O que gostaria de fazer:");
     		// Lista os itens do carrinho.
     		Listagem.listarItensCarrinho(cart);
     		Listagem.pularLinha(1);
-    		System.out.println("1 - Remover determinado produto\n2 - Finalizar compras\n3 - Cancelar carrinho\n0 - Voltar ao menu principal.");
+    		System.out.println("1 - Finalizar compras\n2 - Adicionar determinado produto"
+    							+ "\n3 - Remover determinado produto\n4 - Cancelar carrinho\n0 - Voltar ao menu principal.");
     		
-    		nav = Integer.parseInt(scanUser.nextLine());
-
-    		switch (nav) {
-	    		case 1: // Altera produtos do carrinho.
-	    			int opcao = 0;
-	    			ItemCarrinho itemCart = null;
-	    			Item itemEstoque = null;
-	    			System.out.println("Escolha o produto que deseja remover:");
-	    			Listagem.listarItensCarrinho(cart);
-	    			opcao = controleEntradaDados(scanUser.nextLine(), 1, cart.size())-1;
-	    			itemCart = cart.get(opcao);
+    		String msgErro = "Não existe essa opção. Tente novamente.";
+    		opcao1 = controleEntradaDados(scanUser.nextLine(), 0, 4, msgErro, msgErro);
+    		
+    		msgErro = "Entrada de dado inválida. Tente novamente:"; 
+    		
+    		switch (opcao1) {
+    			case 0 : // Voltar ao menu principal 
+    				opcao1 = 0; // Seta a opção 1 para zero e volta para o menu principal
+    				break;
+    			
+	    		case 1: // Finaliza compra.
+	    			System.out.println("Deseja realmente finalizar as compras?\n1 - Sim\n2 - Não");
+	    			opcao2 = controleEntradaDados(scanUser.nextLine(), 1, 2, msgErro, msgErro);
 	    			
-	    			
-	    			// Referencia do produto
-	    			int pos = itens.indexOf(itemCart.getItem());
-	    			itemEstoque = itens.get(pos);
-	    			int qtd = itemCart.getItem().getQuantidade();
-	    			
+	    			//Cliente finaliza a compra e é criado 
+	    			if (opcao2 == 1) { 
+	    				Carrinho compraFechada = new Carrinho(cart, null); //Colocar a data no lugar do null
+	    				compras.add(compraFechada); //Adiciona a compra na lista de compras para registro
+	    				cart.clear(); // Limpa o cart que é o "carrinho" que o cliente estava usando
+	    				System.out.println("Arigatou Gozaimasu pela compra");
+	    				opcao1 = 0; //Volta para menu principal
+	    			}
 	    			break;
 	    		
-	    		case 2: // O usuario faz a compra finalizando o carrinho.
-	    			
+	    		case 2: // Adiciona unidades do item escolhido no carrinho.
+	    			while (true) {
+	    				System.out.println("Escolha o produto que deseja adicionar unidades:");
+		    			System.out.println("0 - Voltar");
+		    			Listagem.listarItensCarrinho(cart);
+		    			opcao2 = controleEntradaDados(scanUser.nextLine(), 0, cart.size(), msgErro, msgErro);
+		    			Listagem.pularLinha(1);
+		    			
+		    			if (opcao2 == 0) { break; } //Sai do loop porque o usuário decidiu não cancelar mais
+		    			
+		    			ItemCarrinho itemCartAdicionar = cart.get(opcao2 - 1); //Pega o itemCarrinho da opção escolhida
+		    			Item itemRetirarEstoque = itemCartAdicionar.getItem(); //Pega o item para debitar as unidades no estoque
+		    			
+		    			//Tem estoque então pode adicionar mais itens no carrinho
+		    			if (itemRetirarEstoque.getQuantidade() > 0) {
+		    				System.out.println("Informações do item no estoque:\n" + itemRetirarEstoque + "\n");
+		    				System.out.println("Escolha a quantidade que deseja adicionar:");
+			    			String msgErro2 = "Não se pode adicionar com a quantidade inserida";
+			    			quantidade = controleEntradaDados(scanUser.nextLine(), 1, itemRetirarEstoque.getQuantidade(), msgErro, msgErro2);
+			    			
+			    			// Basta adicionar no carrinho e remover do estoque de item
+			    			itemCartAdicionar.adicionaQtdItens(quantidade);
+			    			itemRetirarEstoque.debitarEstoque(quantidade);
+		    			}
+		    			else 
+		    				System.out.println("Não tem como adicionar mais unidades desse produto, pois está indisponível no estoque");
+	    			}
 	    			break;
 	    		
-	    		case 3: // Encerra o carrinho e retorna o estoque dos produtos.
-	    			
+	    		case 3: // Remove unidades do item escolhido no carrinho.
+	    			while (true) {
+	    				System.out.println("Escolha o produto que deseja remover unidades:");
+		    			System.out.println("0 - Voltar");
+		    			Listagem.listarItensCarrinho(cart);
+		    			opcao2 = controleEntradaDados(scanUser.nextLine(), 0, cart.size(), msgErro, msgErro);
+		    			Listagem.pularLinha(1);
+		    			
+		    			if (opcao2 == 0) { break; } //Sai do loop porque o usuário decidiu não cancelar mais
+		    			
+		    			ItemCarrinho itemCartRemover = cart.get(opcao2 - 1); //Pega o itemCarrinho da opção escolhida
+		    			Item itemReporEstoque = itemCartRemover.getItem(); //Pega o item para colocar as unidades no estoque novamente
+		    			
+		    			System.out.println("Escolha a quantidade que deseja remover:");
+		    			String msgErro2 = "Não se pode remover com a quantidade inserida";
+		    			quantidade = controleEntradaDados(scanUser.nextLine(), 1, itemCartRemover.getQuantidade(), msgErro, msgErro2);
+		    			
+		    			// Basta remover do carrinho e adicionar novamente no estoque do item
+		    			itemCartRemover.removeQtdItens(quantidade);
+		    			itemReporEstoque.inserirEstoque(quantidade);
+		    			
+		    			// Deve checar se a unidade do item removido do carrinho está zero. Logo ele deve sair do carrinho
+		    			if (itemCartRemover.getQuantidade() == 0)
+		    				cart.remove(opcao2 - 1); // Remove a referência do carrinho através do index
+	    			}
 	    			break;
 	    			
-	    		default:
-	    			System.out.println("Nao existe essa busca");
+	    		case 4: // Cancelar o carrinho de compras
+	    			System.out.println("Deseja realmente cancelar suas compras?\n1 - Sim\n2 - Não");
+	    			opcao2 = controleEntradaDados(scanUser.nextLine(), 1, 2, msgErro, msgErro);
+	    			
+	    			// Caso o usuário final deseje realmente cancelar é dado um clear no carrinho 
+	    			if (opcao2 == 1) { 
+	    				cart.clear();
+	    				System.out.println("Carrinho cancelado com sucesso.");
+	    				opcao1 = 0; // Volta ao menu principal
+	    			}
+	    			break;
     		}
-    	}while(nav != 0);
+    	} while(opcao1 != 0);
     }
     
+    
+
+    
     //Método que serve para controlar entrada de dados realizada pelo usuário para que digite o correto
-    private static int controleEntradaDados(String opcaoEntrada, int min, int max)  
+    private static int controleEntradaDados(String opcaoEntrada, int min, int max, String msg1, String msg2)  
     {
     	boolean condicao = true;
     	int opcao = 0;
@@ -325,7 +412,7 @@ public class MainApp {
     		} 
     		catch (NumberFormatException e) 
     		{
-    			System.out.println("Entrada de dado inválida. Tente novamente:");
+    			System.out.println(msg1);
     			condicao = true;
     			opcaoEntrada = scanUser.nextLine();
     		}
@@ -335,7 +422,7 @@ public class MainApp {
         		if (condicao == false) {
         			if (opcao < min || opcao > max) {
         				condicao = true; //Opção errada então volta a pedir ao usuário digitar novamente
-        				System.out.println("Entrada de dado inválida. Tente novamente:");
+        				System.out.println(msg2);
         				opcaoEntrada = scanUser.nextLine();
         			}
         		} 
@@ -343,6 +430,11 @@ public class MainApp {
     	}
     	
     	return opcao;
-    } 
+    }
+    
+    // Serializar tudo que foi feito
+    public static void serializacaoInformacoes() {
+    	//TODO - fazer aqui
+    }
 }
 	
--- StoresProject_version2/src/modelStore/ItemCarrinho.java
+++ StoresProject_version2/src/modelStore/ItemCarrinho.java
@@ -6,7 +6,8 @@ public class ItemCarrinho {
 	private Item item;
 	
 	/* Construtor */
-	public ItemCarrinho(Item item, int quantidade) {
+	public ItemCarrinho(Item item, int quantidade) 
+	{
 		this.item = item;
 		this.quantidade = quantidade;
 	}
@@ -27,5 +28,39 @@ public class ItemCarrinho {
 	{
 		return this.item.getLoja().getId() + " " + this.getItem().getProduto() + String.format("%-3d %.2f", 
 				this.quantidade, this.item.getPreco());
+	}
+	
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (obj == null || !(obj instanceof ItemCarrinho))
+			return false;
+		
+		if (this.item.equals( ((ItemCarrinho)obj).getItem() ))
+			return true;
+		
+		return false;
+	}
+	
+	/* Adiciona mais unidades itens caso para o determinado item carrinho já existente */
+	public boolean adicionaQtdItens(int quantidade)
+	{
+		if (quantidade > 0 && item.getQuantidade() - quantidade >= 0) {
+			this.quantidade += quantidade;
+			return true;
+		}
+			
+		return false;
+	}
+	
+	/* Remove unidades itens caso para o determinado item carrinho já existente */
+	public boolean removeQtdItens(int quantidade) 
+	{
+		if (quantidade > 0 && quantidade <= this.quantidade) {
+			this.quantidade -= quantidade;
+			return true;
+		} 
+		
+		return false;
 	}
 }
